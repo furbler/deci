f92c5420bd98202b0b89999d3be69304731091ee
furbler <63544661+furbler@users.noreply.github.com>
2023-04-26 21:28:08 +0900

    default-editor
    editor関係の処理を別ファイルに分離

---
+++ src/editor.rs
@@ -0,0 +1,43 @@
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        for key in io::stdin().keys() {
+            match key {
+                Ok(key) => match key {
+                    // 任意の文字
+                    Key::Char(c) => {
+                        let b = c as u8;
+                        if c.is_control() {
+                            // 2進数, 10進数
+                            println!("{:08b}, {:?}\r", b, b);
+                        } else {
+                            // 2進数, 10進数 (文字)
+                            println!("{:08b}, {:?} ({})\r", b, b, c);
+                        }
+                    }
+                    // Ctrl-q
+                    Key::Ctrl('q') => break,
+                    // 上記以外
+                    _ => println!("{:?}\r", key),
+                },
+                Err(err) => die(err),
+            }
+        }
+    }
+
+    pub fn default() -> Self {
+        Editor {}
+    }
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,7 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+mod editor;
 
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
-
+use editor::Editor;
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                // 任意の文字
-                Key::Char(c) => {
-                    let b = c as u8;
-                    if c.is_control() {
-                        // 2進数, 10進数
-                        println!("{:08b}, {:?}\r", b, b);
-                    } else {
-                        // 2進数, 10進数 (文字)
-                        println!("{:08b}, {:?} ({})\r", b, b, c);
-                    }
-                }
-                // Ctrl-q
-                Key::Ctrl('q') => break,
-                // 上記以外
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => die(err),
-        }
-    }
+    let editor = Editor::default();
+    editor.run();
 }
