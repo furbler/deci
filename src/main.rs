dc1fc485bc402fd56cb04b4cc586d19b0dcab526
furbler <63544661+furbler@users.noreply.github.com>
2023-04-26 11:55:45 +0900

    die-on-input-error
    標準入力読み込み時にエラーが出たらパニックする

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,30 @@ fn to_ctrl_byte(c: char) -> u8 {
     let byte = c as u8;
     byte & 0b0001_1111
 }
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
 
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            // 2進数, 10進数
-            println!("{:08b}, {:?}\r", b, b);
-        } else {
-            // 2進数, 10進数 (文字)
-            println!("{:08b}, {:?} ({})\r", b, b, c);
-        }
+        match b {
+            Ok(b) => {
+                let c = b as char;
 
-        if b == to_ctrl_byte('q') {
-            break;
+                if c.is_control() {
+                    // 2進数, 10進数
+                    println!("{:08b}, {:?}\r", b, b);
+                } else {
+                    // 2進数, 10進数 (文字)
+                    println!("{:08b}, {:?} ({})\r", b, b, c);
+                }
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
         }
     }
 }
