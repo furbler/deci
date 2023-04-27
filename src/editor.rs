5688fa69b658a708ba7b7dd97d9cf14a8aeadc8e
furbler <63544661+furbler@users.noreply.github.com>
2023-04-27 23:51:41 +0900

    reading-evaluating
    キーを取得する関数を変更して、入力されたキーの表示処理を削除

--- src/editor.rs
+++ src/editor.rs
@@ -8,36 +8,31 @@ pub struct Editor {}
 impl Editor {
     pub fn run(&self) {
         let _stdout = stdout().into_raw_mode().unwrap();
-
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    // 任意の文字
-                    Key::Char(c) => {
-                        let b = c as u8;
-                        if c.is_control() {
-                            // 2進数, 10進数
-                            println!("{:08b}, {:?}\r", b, b);
-                        } else {
-                            // 2進数, 10進数 (文字)
-                            println!("{:08b}, {:?} ({})\r", b, b, c);
-                        }
-                    }
-                    // Ctrl-q
-                    Key::Ctrl('q') => break,
-                    // 上記以外
-                    _ => println!("{:?}\r", key),
-                },
-                Err(err) => die(err),
+        loop {
+            if let Err(error) = self.process_keypress() {
+                die(error);
             }
         }
     }
-
     pub fn default() -> Self {
         Self {}
     }
+    fn process_keypress(&self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => panic!("Program end"),
+            _ => (),
+        }
+        Ok(())
+    }
 }
-
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+    }
+}
 fn die(e: std::io::Error) {
     panic!("{}", e);
 }
