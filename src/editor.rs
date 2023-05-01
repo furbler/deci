c232dba25a1a045b182250bcc906840fb0abb634
furbler <63544661+furbler@users.noreply.github.com>
2023-05-01 22:57:19 +0900

    ignore-last-line
    一番下の行にチルダを表示しない

--- src/editor.rs
+++ src/editor.rs
@@ -52,7 +52,7 @@ impl Editor {
     }
     // 行頭にチルダを表示
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height {
+        for _ in 0..self.terminal.size().height-1 {
             println!("~\r");
         }
     }
