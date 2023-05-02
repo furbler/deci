28447a1578c2bc44d05043e09b69e027554284b5
furbler <63544661+furbler@users.noreply.github.com>
2023-05-02 11:41:32 +0900

    navigate-within-bounds
    カーソルが画面外に出るのを防ぐ

--- src/editor.rs
+++ src/editor.rs
@@ -63,11 +63,22 @@ impl Editor {
     // 入力したキーに応じてカーソル移動
     fn move_cursor(&mut self, key: Key) {
         let Position { mut y, mut x } = self.cursor_position;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
         match key {
             Key::Up | Key::Char('k') => y = y.saturating_sub(1),
-            Key::Down | Key::Char('j') => y = y.saturating_add(1),
+            Key::Down | Key::Char('j') => {
+                if y < height {
+                    y = y.saturating_add(1);
+                };
+            }
             Key::Left | Key::Char('h') => x = x.saturating_sub(1),
-            Key::Right | Key::Char('l') => x = x.saturating_add(1),
+            Key::Right | Key::Char('l') => {
+                if x < width {
+                    x = x.saturating_add(1);
+                };
+            }
             _ => (),
         }
         self.cursor_position = Position { x, y }
